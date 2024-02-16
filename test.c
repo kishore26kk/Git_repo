a943bbd1d5d615c88a3b1249fb5dac484afd31d8
Chandra Sekar <kchand732@apac.comcast.com>
2024-02-16 12:50:18 +0530

    Added new code to the file
    Signed-off-by: Chandra Sekar <kchand732@apac.comcast.com>

--- test.c
+++ test.c
@@ -1,9 +1,12 @@
 #include<Stdio.h>
 #include<String.h>
+
 int main(){
-	int a = 10;
-	for(int i=0;i<a;i++){
-		printf("%d",i*2);
+	int a=10;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<a;j++){
+			printf("%d",i+j);
+		}
 	}
 	return 0;
 }
